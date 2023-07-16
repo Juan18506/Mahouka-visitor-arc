6fc6ad390f540b8c29ac3df3455811288857a6a4
juan18506 <juan185067@gmail.com>
2023-07-16 07:00:17 -0300

    added other animations for logo,  nav and imgs

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
-addEventListener('DOMContentLoaded', () => {
-  document.getElementById('ppal').style.animation = 'main_img_animation 60s linear'
-})
+// addEventListener('DOMContentLoaded', () => {
+//   document.getElementById('logo').style.animation = 'logo_animation 1.5s ease-in'
+//   document.getElementById('ppal').style.animation = 'main_img_animation 60s linear'
+// })
