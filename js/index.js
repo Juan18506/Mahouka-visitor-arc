d7a67bd0581aa6af82d1e792632efeb7861a47ca
juan18506 <juan185067@gmail.com>
2023-07-16 23:00:35 -0300

    added modal video logic (no animations)

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
-// addEventListener('DOMContentLoaded', () => {
-//   document.getElementById('logo').style.animation = 'logo_animation 1.5s ease-in'
-//   document.getElementById('ppal').style.animation = 'main_img_animation 60s linear'
-// })
+addEventListener('DOMContentLoaded', () => {
+  const modal = document.getElementById('modal')
+
+  document.getElementById('img-center').addEventListener('animationend', () => {
+    modal.style.display = 'block'
+  })
+  
+  document.getElementById('video').addEventListener('click', () => {
+    modal.style.display = 'block'
+    
+  })
+
+  document.getElementById('close_btn').addEventListener('click', () => {
+    modal.style.display = 'none'
+
+    const iframe = document.getElementById('modal__iframe')
+    iframe.src = iframe.src
+  })
+})
