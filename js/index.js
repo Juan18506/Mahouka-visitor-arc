a51f03592bd78efb5323408b17c7a90a18404154
juan18506 <juan185067@gmail.com>
2023-07-21 06:01:53 -0300

    responsive design ( - nav )

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 addEventListener('DOMContentLoaded', () => {
-  const modal = document.getElementById('modal')
-  const iframe = document.getElementById('modal__iframe')
+  const modal = document.querySelector('.modal')
+  const iframe = modal.querySelector('.modal__iframe')
 
-  document.getElementById('img-center').addEventListener('animationend', () => {
+  document.querySelector('.ppal__img--center').addEventListener('animationend', () => {
     modal.style.display = 'block'
   })
   
-  document.getElementById('video').addEventListener('click', () => {
+  document.querySelector('.streaming__border').addEventListener('click', () => {
     modal.style.display = 'block'
   })
 
-  document.getElementById('close_btn').addEventListener('click', () => {
+  document.querySelector('.modal__close').addEventListener('click', () => {
     modal.style.display = 'none'
     iframe.src = iframe.src
   })
