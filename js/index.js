2c351cc1f4a513a1309378dfcf7c5ab5ece149f6
juan18506 <juan185067@gmail.com>
2023-07-21 21:06:46 -0300

    added nav responsive

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 addEventListener('DOMContentLoaded', () => {
   const modal = document.querySelector('.modal')
   const iframe = modal.querySelector('.modal__iframe')
+  const header = document.querySelector('.header')
+  const openBtn = header.querySelector('.header__open')
+  const closeBtn = header.querySelector('.header__close')
+  const nav = header.querySelector('.header__nav')
 
   document.querySelector('.ppal__img--center').addEventListener('animationend', () => {
     modal.style.display = 'block'
@@ -13,6 +17,18 @@ addEventListener('DOMContentLoaded', () => {
   document.querySelector('.modal__close').addEventListener('click', () => {
     modal.style.display = 'none'
     iframe.src = iframe.src
+  })
+
+  openBtn.addEventListener('click', () => {
+    openBtn.style.display = 'none'
+    closeBtn.style.display = 'block'
+    nav.style.display = 'block'
+  })
+
+  closeBtn.addEventListener('click', () => {
+    nav.style.display = 'none'
+    closeBtn.style.display = 'none'
+    openBtn.style.display = 'flex'
   })
 
   modal.addEventListener('click', () => {
