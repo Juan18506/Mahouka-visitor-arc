da82b5f8c0f8408ef923f1c5f65d476d8c54e4d3
juan18506 <juan185067@gmail.com>
2023-07-23 05:57:46 -0300

    added header the actual page

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
 'use strict'
 
-console.log('Hola Mundo')
+document.addEventListener('DOMContentLoaded', () => {
+  const headerAnchors = document.querySelectorAll('.header__a')
+
+  const handleHeaderAnchors = event => {
+    const { target } = event
+
+    headerAnchors.forEach(anchor => {
+      anchor.classList.remove('header__a--active')
+    })
+
+    target.classList.add('header__a--active')
+  }
+
+  headerAnchors.forEach(anchor => {
+    anchor.addEventListener('pointerdown', handleHeaderAnchors)
+  })
+})
