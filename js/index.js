299fc077c49d055e94e945f294181d43d3b8d9f7
juan18506 <juan185067@gmail.com>
2023-07-23 23:59:46 -0300

    finished win ver + minify

--- js/index.js
+++ js/index.js
@@ -2,18 +2,35 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   const headerAnchors = document.querySelectorAll('.header__a')
+  const modal = document.querySelector('.modal')
+  const modalBtn = document.querySelector('.modal__btn')
+  const iframe = modal.querySelector('.modal__iframe')
+  const streamingIframe = document.querySelector('.streaming__border')
 
-  const handleHeaderAnchors = event => {
+  const handleHeader = (event) => {
     const { target } = event
 
-    headerAnchors.forEach(anchor => {
+    headerAnchors.forEach((anchor) => {
       anchor.classList.remove('header__a--active')
     })
 
     target.classList.add('header__a--active')
   }
+  const handleIframes = () => {
+    if (modal.classList.contains('modal--inactive')) {
+      modal.classList.remove('modal--inactive')
+    } else {
+      modal.classList.add('modal--inactive')
+      iframe.src = iframe.src
+    }
+  }
 
-  headerAnchors.forEach(anchor => {
-    anchor.addEventListener('pointerdown', handleHeaderAnchors)
+  headerAnchors.forEach((anchor) => {
+    anchor.addEventListener('click', handleHeader)
   })
+
+  const modalChangingElements = [streamingIframe, modalBtn]
+  modalChangingElements.forEach((e) =>
+    e.addEventListener('click', handleIframes)
+  )
 })
