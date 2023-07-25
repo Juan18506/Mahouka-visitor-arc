18c81e8979a6540950e418957c3fb15e296c71d6
juan18506 <juan185067@gmail.com>
2023-07-24 23:01:16 -0300

    finished responsive

--- js/index.js
+++ js/index.js
@@ -1,20 +1,37 @@
 'use strict'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const headerAnchors = document.querySelectorAll('.header__a')
+  const headerNav = document.querySelector('.header__nav')
+  const headerAnchors = headerNav.querySelectorAll('.header__a')
+  const headerButtons = document.querySelector('.header__buttons')
+  const btnClose = headerButtons.querySelector('.buttons__btn--close')
+  const btnOpen = headerButtons.querySelector('.buttons__btn--open')
   const modal = document.querySelector('.modal')
-  const modalBtn = document.querySelector('.modal__btn')
+  const modalBtn = modal.querySelector('.modal__btn')
   const iframe = modal.querySelector('.modal__iframe')
   const streamingIframe = document.querySelector('.streaming__border')
 
-  const handleHeader = (event) => {
-    const { target } = event
+  const handleHeaderAnchors = (event) => {
+    const { target: anchor } = event
 
     headerAnchors.forEach((anchor) => {
       anchor.classList.remove('header__a--active')
     })
 
-    target.classList.add('header__a--active')
+    anchor.classList.add('header__a--active')
+  }
+  const handleHeaderButtons = (event) => {
+    const { target: btn } = event
+  
+    if (btn.classList.contains('buttons__btn--open')) {
+      headerNav.classList.remove('header__nav--inactive')
+      btnOpen.classList.add('buttons__btn--inactive')
+      btnClose.classList.remove('buttons__btn--inactive')
+    } else {
+      headerNav.classList.add('header__nav--inactive')
+      btnOpen.classList.remove('buttons__btn--inactive')
+      btnClose.classList.add('buttons__btn--inactive')
+    }
   }
   const handleIframes = () => {
     if (modal.classList.contains('modal--inactive')) {
@@ -26,11 +43,15 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   headerAnchors.forEach((anchor) => {
-    anchor.addEventListener('click', handleHeader)
+    anchor.addEventListener('click', handleHeaderAnchors)
   })
+
+  const headerBtnArray = [btnOpen, btnClose]
+  headerBtnArray.forEach(btn => btn.addEventListener('click', handleHeaderButtons))
 
   const modalChangingElements = [streamingIframe, modalBtn]
   modalChangingElements.forEach((e) =>
     e.addEventListener('click', handleIframes)
   )
+
 })
