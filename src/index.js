6611c58af77a2d1fa14ddf449ff1361fa2eaba4d
juan18506 <juan185067@gmail.com>
2023-09-11 17:33:43 -0300

    moved handler functions outside DOMContentLoaded event

--- src/index.js
+++ src/index.js
@@ -1,48 +1,49 @@
 'use strict';
 
+const handleNav = ({ target }) => {
+  if (!target.classList.contains('header__a')) return;
+
+  const headerAnchors = document.querySelectorAll('.header__a');
+  headerAnchors.forEach((anchor) => {
+    anchor.classList.remove('header__a--active');
+  });
+  
+  target.classList.add('header__a--active');
+}
+
+const handleButtons = () => {
+  const headerNav = document.querySelector('.header__nav');
+  headerNav.classList.toggle('header__nav--inactive');
+
+  const btns = [ ...headerNav.previousElementSibling.children ];
+  btns.forEach((btn) => {
+    btn.classList.toggle('buttons__btn--inactive');
+  });
+}
+
+const handleIframe = ({ target }) => {
+  const modal  = document.querySelector('.modal');
+  const iframe = modal.firstElementChild;
+
+  if (!modal.classList.contains('modal--inactive')) iframe.src = iframe.src;
+  modal.classList.toggle('modal--inactive');
+
+  target.removeEventListener('animationend', handleIframe);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  const header          = document.querySelector( '.header' );
-  const headerNav       = header.querySelector( '.header__nav' );
-  const headerAnchors   = headerNav.querySelectorAll( '.header__a' );
-  const headerButtons   = header.querySelector( '.header__buttons' );
-  const btnClose        = headerButtons.querySelector( '.buttons__btn--close' );
-  const btnOpen         = headerButtons.querySelector( '.buttons__btn--open' );
-  const centeredImg     = document.querySelector( '.hero__img--center' );
-  const modal           = document.querySelector( '.modal' );
-  const modalBtn        = modal.querySelector( '.modal__btn' );
-  const iframe          = modal.querySelector( '.modal__iframe' );
-  const streamingIframe = document.querySelector( '.streaming__border' );
+  const header                        = document.querySelector('.header');
+  const [ headerButtons, headerNav ]  = header.children;
+  const centeredImg                   = document.querySelector('.hero__img--center');
+  const modalChangingElements         = [ 
+    document.querySelector('.streaming__border'),
+    document.querySelector('.modal__btn'),
+  ];
 
-  const handleIframe = () => {
-    if (modal.classList.contains('modal--inactive')) {
-      modal.classList.remove('modal--inactive');
-    } else {
-      modal.classList.add('modal--inactive');
-      iframe.src = iframe.src;
-    }
-
-    centeredImg.removeEventListener('animationend', handleIframe);
-  }
-
-  headerNav.addEventListener('click', ({ target }) => {
-    if (!target.classList.contains('header__a')) return;
-    headerAnchors.forEach((a) => {
-      a.classList.remove('header__a--active');
-    });
-    
-    target.classList.add('header__a--active');
-  });
-
-  headerButtons.addEventListener('click', () => {
-    headerNav.classList.toggle('header__nav--inactive');
-    btnOpen.classList.toggle('buttons__btn--inactive');
-    btnClose.classList.toggle('buttons__btn--inactive');
-  });
-
-  const modalChangingElements = [streamingIframe, modalBtn];
-  modalChangingElements.forEach(( el ) =>
-    el.addEventListener('click', handleIframe)
-  );
-
+  headerNav.addEventListener('click', handleNav);
+  headerButtons.addEventListener('click', handleButtons);
   centeredImg.addEventListener('animationend', handleIframe);
+  modalChangingElements.forEach((el) => {
+    el.addEventListener('click', handleIframe);
+  });
 });
